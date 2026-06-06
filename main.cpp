dcc18e474e0f3a0f3e0dd99fdb2bc939aa0fa839
Ckonst00 <aleksi.kaurila@gmail.com>
2026-06-06 11:53:34 +0300

    Add: Read Json and create files from it. Output is directed to output folder

--- main.cpp
+++ main.cpp
@@ -1,36 +1,58 @@
+#include <cassert>
 #include <cstring>
+#include <filesystem>
 #include <fstream>
 #include <iostream>
 #include <nlohmann/json.hpp>
 #include <sstream>
 #include <string>
+#include <string_view>
 #include <vector>
 
-int toJson(const std::vector<std::string>& inputs) {
-  
+int createFile(const std::string_view filename, const std::string_view content) {
+
+  std::filesystem::path outDir = std::filesystem::current_path() / "output";
+  std::cout << "Output dir: " << outDir << "\n";
+  std::filesystem::create_directories(outDir);
+  std::filesystem::path filepath = outDir / std::string(filename);
+  std::cout << "Creating file: " << filepath << "\n";
+
+  // Open a file
+  std::ofstream outf{filepath};
+  if (!outf) {
+    std::cerr << "Could not open " << std::string(filename) << "\n";
+    return 1;
+  }
+
+  outf << content;
+
+  return 0;
+}
+
+int toJson(const std::vector<std::string> &inputs) {
+
   // Print out the given filenames
   std::cout << "> Input arguments: ";
   for (auto &input : inputs) {
     std::cout << input << " ";
   }
   std::cout << "\n";
-  
+
   std::string JsonFileName{inputs[1]};
   JsonFileName.append(".json");
-  
-  
-  
+
   std::ofstream outf{JsonFileName}; // open the json file
   if (!outf) {
     std::cerr << "Could not open " << JsonFileName << "\n";
     return 1;
   }
-  
+
   nlohmann::ordered_json files = nlohmann::ordered_json::array();
-  
+
   // Inputs[0] = I
   // Inputs[1] = json filename
-  for (int i = 2; i < inputs.size(); i++) { // loop to open and read the given files
+  // loop to open and read the given files
+  for (int i = 2; i < inputs.size(); i++) {
     std::ifstream inf(inputs[i]);
     if (!inf) {
       std::cerr << "Could not open: " << inputs[i] << "\n";
@@ -51,12 +73,43 @@ int toJson(const std::vector<std::string>& inputs) {
   }
 
   outf << files;
+  std::cout << "> " << inputs[1] << ".json succesfully created.\n";
   return 0;
 }
 
+int fromJson(const std::vector<std::string> &inputs) {
+
+  // Print out the given filenames
+  std::cout << "> Input arguments: ";
+  for (auto &input : inputs) {
+    std::cout << input << " ";
+  }
+  std::cout << "\n";
+
+  std::string JsonFileName{inputs[1]};
+  JsonFileName.append(".json");
+
+  std::ifstream inf{JsonFileName}; // open the json file
+  if (!inf) {
+    std::cerr << "Could not open " << JsonFileName << "\n";
+    return 1;
+  }
+
+  try {
+    nlohmann::json data = nlohmann::json::parse(inf);
+    for (auto &entry : data) {
+      createFile(entry["filename"].get<std::string>(),
+                 entry["content"].get<std::string>());
+    }
+
+  } catch (const nlohmann::json::parse_error &e) {
+    std::cerr << "Invalid JSON: " << e.what() << "\n";
+  }
+
+  return 0;
+}
 
 int main(int argc, char *argv[]) {
-
 
   std::vector<std::string> inputs; // input  storage
   for (int i = 1; i < argc; i++) {
@@ -65,10 +118,13 @@ int main(int argc, char *argv[]) {
 
   if (strcmp(argv[1], "I") == 0) {
     toJson(inputs);
+
+  } else if (strcmp(argv[1], "O") == 0) {
+    fromJson(inputs);
+
   } else {
     std::cerr << "> Incorrect arguments.\n";
   }
-
 
   return 0;
 }
